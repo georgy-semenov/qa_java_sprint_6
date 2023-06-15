0d2237889e7dc4e35ddf58077508ef434dd19963
Georgy Semenov <snejsafina@yandex.ru>
2023-06-15 17:47:11 +0500

    commit classes

---
+++ src/main/java/org/example/Lion.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.List;
+
+public class Lion {
+    private boolean hasMane;
+    private Feline feline;
+
+    public Lion(String sex, Feline feline) throws Exception {
+        if ("Самец".equals(sex)) {
+            hasMane = true;
+            this.feline = feline;
+        } else if ("Самка".equals(sex)) {
+            hasMane = false;
+            this.feline = feline;
+        } else {
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
+        }
+    }
+
+    public int getKittens() {
+        return feline.getKittens();
+    }
+
+    public boolean doesHaveMane() {
+        return hasMane;
+    }
+
+    public List<String> getFood() throws Exception {
+        return feline.getFood("Хищник");
+    }
+}
+
