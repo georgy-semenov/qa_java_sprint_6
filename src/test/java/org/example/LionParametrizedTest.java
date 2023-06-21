f493d516eb35c3d84cb917a507b7420b37cee0b3
Georgy Semenov <snejsafina@yandex.ru>
2023-06-22 00:58:40 +0500

    commit

--- src/test/java/org/example/LionParametrizedTest.java
+++ src/test/java/org/example/LionParametrizedTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -11,19 +12,19 @@ import static org.junit.Assert.*;
 @RunWith(Parameterized.class)
 public class LionParametrizedTest {
     private final String sex;
-    private final boolean expectedResult;
 
-    public LionParametrizedTest(String sex, boolean expectedResult) {
+
+    public LionParametrizedTest(String sex) {
         this.sex = sex;
-        this.expectedResult = expectedResult;
+
     }
 
     @Parameterized.Parameters(name = "Гендер. Тестовые данные: {0}")
     public static Object[][] createParametrsOfLion() {
         return new Object[][]{
-                {"Собака", true},
-                {"null", false},
-                {"Львенок", false},
+                {"Собака"},
+                {"null"},
+                {"Львенок"},
         };
     }
 
@@ -34,7 +35,6 @@ public class LionParametrizedTest {
     public void testCreateFailLion(){
         try {
             Lion lion = new Lion(sex, feline);
-            assertEquals(expectedResult, lion.doesHaveMane());
         } catch (Exception exception) {
             assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
         }
